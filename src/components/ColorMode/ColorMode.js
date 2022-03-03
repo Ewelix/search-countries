72b0d1f658e04e6fb993ecfe5152382b3f7601d9
Ewe <ewelina.polewka1@gmail.com>
2022-03-03 16:52:19 +0100

    Basic project structure added

---
+++ src/components/ColorMode/ColorMode.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ColorMode = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ColorMode;
---
+++ src/components/CountriesListItem/CountriesListItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountriesListItem = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CountriesListItem;
