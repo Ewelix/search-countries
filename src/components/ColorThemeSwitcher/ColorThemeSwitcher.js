e79d8d1a2ae25ac9097b02af247059b1b5a06e38
Ewe <ewelina.polewka1@gmail.com>
2022-03-09 17:17:36 +0100

    Mode switch logic added

--- src/components/ColorThemeSwitcher/ColorThemeSwitcher.js
+++ src/components/ColorThemeSwitcher/ColorThemeSwitcher.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { FaMoon } from 'react-icons/fa';
 import { FaRegMoon } from 'react-icons/fa';
-import { Wrapper } from "./ColorThemeSwitcher.styles";
-import {useDarkModeContext} from "../../providers/darkModeProvider";
+import { Button } from "./ColorThemeSwitcher.styles";
+import { useDarkModeContext } from "../../providers/darkModeProvider";
 
 const ColorThemeSwitcher = () => {
-    const { mode } = useDarkModeContext();
+    const { mode, toggleMode } = useDarkModeContext();
+
+    const handleClick = () => {
+        toggleMode(mode === 'dark' ? 'light' : 'dark')
+    }
 
     return (
-        <Wrapper>
+        <Button onClick={handleClick}>
             {mode === 'dark'? <FaMoon /> : <FaRegMoon />}
              Dark Mode
-        </Wrapper>
+        </Button>
     );
 };
 
--- src/components/ColorThemeSwitcher/ColorThemeSwitcher.styles.js
+++ src/components/ColorThemeSwitcher/ColorThemeSwitcher.styles.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const Button = styled.button`
   display: flex;
   align-items: center;
   gap: 10px;
   font-size: ${({theme}) => theme.fontSize.xs};
+  background: inherit;
+  color: inherit;
+  border: none;
+  cursor: pointer;
 
   @media (${({ theme }) => theme.device.s}) {
     font-size: ${({theme}) => theme.fontSize.s};
