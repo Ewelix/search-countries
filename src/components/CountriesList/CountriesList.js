bfe804712f703b6c525f126a5b0ea3a7c41ec7d5
Ewe <ewelina.polewka1@gmail.com>
2022-03-15 14:22:40 +0100

    Loading text added

--- src/components/CountriesList/CountriesList.js
+++ src/components/CountriesList/CountriesList.js
@@ -7,15 +7,16 @@ const API_URL = 'https://restcountries.com/v2';
 
 const CountriesList = () => {
     const [countries, setCountries] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch(`${API_URL}/all`)
             .then(response => response.json())
             .then(data => {
                 setCountries(data);
+                setIsLoading(false);
             })
             .catch(err => console.log(err))
-
     }, []);
 
     return (
@@ -23,7 +24,7 @@ const CountriesList = () => {
             <Container>
             {countries.length ? (
                 countries.map(country => <CountriesListItem country={country} key={country.name}/>)
-            ) : <h2>No countries to display.</h2>}
+            ) : <h2>{isLoading ? 'Loading...' : 'No countries to display.'}</h2>}
             </Container>
         </Wrapper>
     );
